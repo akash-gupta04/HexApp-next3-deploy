b200425c4e87178f1418f0949bb07c2693a03f5c
Akash Gupta <78719937+akash-gupta04@users.noreply.github.com>
2024-12-04 13:52:12 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,21 +11,23 @@ export default function Page() {
   const [selectedHexes, setSelectedHexes] = useState<string[]>([]);
   const [resolution, setResolution] = useState(5);
 
-  const hexagonAreaPerResolution = [
-    4357449, // km² at resolution 0
-    1538395,
-    219017,
-    31359,
-    4479,
-    639,
-    91,
-    13,
-    2,
-    0.3,
-    0.04, // km² at resolution 10
+  const hexagonEdgeLengthPerResolution = [
+    1107712.591, // km at resolution 0
+    418676.0055,
+    158244.6558,
+    59810.85794,
+    22625.43656,
+    8556.27732,
+    3237.663461,
+    1224.837918,
+    463.304065,
+    175.6618745,
+    66.51154506,
+    25.16993739,
+    9.519281662,
+    3.599244768,
+    1.361999115, // km at resolution 14
   ];
-
-  const totalSelectedArea = selectedHexes.length * hexagonAreaPerResolution[resolution];
 
   return (
     <main className="p-4">
@@ -41,19 +43,15 @@ export default function Page() {
             onChange={(e) => setResolution(Number(e.target.value))}
             className="p-2 border rounded"
           >
-            {Array.from({ length: 11 }, (_, i) => (
+            {Array.from({ length: 15 }, (_, i) => (
               <option key={i} value={i}>
                 Resolution {i}
               </option>
             ))}
           </select>
+          
         </div>
-        <p className="text-sm text-gray-600">
-          <strong>Approx. Area per Hex:</strong>{" "}
-          <span className="mt-2 text-gray-700">
-            {hexagonAreaPerResolution[resolution].toLocaleString()} km²
-          </span>
-        </p>
+      
       </div>
 
       {/* Map Section */}
@@ -81,9 +79,10 @@ export default function Page() {
           }}
           className="w-full p-2 border rounded font-mono text-sm h-28 resize-none overflow-y-auto"
         />
-        <p className="mt-2 text-gray-700">
-          <strong>Total Approx. Area:</strong>{" "}
-          <span className="">{totalSelectedArea.toLocaleString()} km²</span>
+        <p className="text-sm text-gray-600">
+          <strong>Selected Hexagon Edge Size :</strong>{" "}
+          <span className="mt-2 text-gray-700">
+          {hexagonEdgeLengthPerResolution[resolution].toLocaleString()} meters</span>
         </p>
       </div>
     </main>
